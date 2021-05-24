61ca05d8270be1c33c9bcdc1d250fb70d99b5400
zolbayar8027 <zolbayardoll1234@gmail.com>
2021-05-24 11:40:07 +0800

    adsd

--- lib/responsive_widget.dart
+++ lib/responsive_widget.dart
@@ -1,12 +1,11 @@
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:flutter_web/material.dart';
 
-class ResponsiveLayout extends StatelessWidget {
+class ResponsiveWidget extends StatelessWidget {
   final Widget largeScreen;
   final Widget mediumScreen;
   final Widget smallScreen;
 
-  const ResponsiveLayout(
+  const ResponsiveWidget(
       {Key key,
         @required this.largeScreen,
         this.mediumScreen,
@@ -17,25 +16,26 @@ class ResponsiveLayout extends StatelessWidget {
     return MediaQuery.of(context).size.width < 800;
   }
 
-  static bool isMediumScreen(BuildContext context) {
-    return MediaQuery.of(context).size.width > 800 &&
-        MediaQuery.of(context).size.width < 1200;
-  }
-
   static bool isLargeScreen(BuildContext context) {
     return MediaQuery.of(context).size.width > 800;
+  }
+
+  static bool isMediumScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width >= 800 &&
+        MediaQuery.of(context).size.width <= 1200;
   }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        if(constraints.maxWidth > 800)
+        if (constraints.maxWidth > 1200) {
           return largeScreen;
-        if(constraints.maxWidth < 1200 && constraints.maxWidth > 800)
+        } else if (constraints.maxWidth <= 1200 && constraints.maxWidth >= 800) {
           return mediumScreen ?? largeScreen;
-        else
+        } else {
           return smallScreen ?? largeScreen;
+        }
       },
     );
   }
